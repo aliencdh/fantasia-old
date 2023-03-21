12ffa69d7c99811c7bb79d65d146a5b65729cd4a
aliencdh <cdevhax@gmail.com>
2023-03-21 17:03:28 +0100

    implement filling triangles

--- src/drawing_algorithms/mod.rs
+++ src/drawing_algorithms/mod.rs
@@ -1,12 +1,51 @@
+use crate::{Vec3, DIMENSION};
 use image::{DynamicImage, GenericImage};
 
 pub fn line(x0: i32, y0: i32, x1: i32, y1: i32, image: &mut DynamicImage, color: image::Rgba<u8>) {
-    for t in 0..100 {
+    for t in 0..DIMENSION {
         // graph the line by "traveling" along its trajectory
-        let t = t as f32 / 100f32;
+        let t = t as f32 / DIMENSION as f32;
         let x = x0 as f32 + (x1 - x0) as f32 * t;
         let y = y0 as f32 + (y1 - y0) as f32 * t;
 
         image.put_pixel(x as u32, y as u32, color);
+    }
+}
+
+pub fn empty_triangle(
+    a: (i32, i32),
+    b: (i32, i32),
+    c: (i32, i32),
+    image: &mut DynamicImage,
+    color: image::Rgba<u8>,
+) {
+    line(a.0 as i32, a.1 as i32, b.0 as i32, b.1 as i32, image, color);
+    line(b.0 as i32, b.1 as i32, c.0 as i32, c.1 as i32, image, color);
+    line(c.0 as i32, c.1 as i32, a.0 as i32, a.1 as i32, image, color)
+}
+
+pub fn filled_triangle(
+    a: (i32, i32),
+    b: (i32, i32),
+    c: (i32, i32),
+    image: &mut DynamicImage,
+    color: image::Rgba<u8>,
+) {
+    let mut vertices = [a, b, c];
+    vertices.sort_by(|x, y| x.1.cmp(&y.1));
+
+    for t in 0..DIMENSION {
+        // rasterize the point on the left and right sides
+        let t = t as f32 / DIMENSION as f32;
+        let x1 = vertices[0].0 as f32 + (vertices[1].0 - vertices[0].0) as f32 * t;
+        let y1 = vertices[0].1 as f32 + (vertices[1].1 - vertices[0].1) as f32 * t;
+        let x2 = vertices[0].0 as f32 + (vertices[2].0 - vertices[0].0) as f32 * t;
+        let y2 = vertices[0].1 as f32 + (vertices[2].1 - vertices[0].1) as f32 * t;
+
+        image.put_pixel(x1 as u32, y1 as u32, color);
+        image.put_pixel(x2 as u32, y2 as u32, color);
+
+        // draw a line between the segments
+        line(x1 as i32, y1 as i32, x2 as i32, y2 as i32, image, color);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,17 @@ mod obj_reader;
 
 const WHITE: image::Rgba<u8> = image::Rgba([255, 255, 255, 255]);
 const RED: image::Rgba<u8> = image::Rgba([255, 0, 0, 255]);
+const GREEN: image::Rgba<u8> = image::Rgba([0, 255, 0, 255]);
 
-const WIDTH: i32 = 1024;
-const HEIGHT: i32 = 1024;
+const DIMENSION: i32 = 1024;
 
 fn main() -> eyre::Result<()> {
     // draw a black image
     let mut image =
-        image::DynamicImage::ImageRgba8(image::RgbaImage::new(WIDTH as u32, HEIGHT as u32));
+        image::DynamicImage::ImageRgba8(image::RgbaImage::new(DIMENSION as u32, DIMENSION as u32));
 
-    for x in 0..(WIDTH as u32) {
-        for y in 0..(HEIGHT as u32) {
+    for x in 0..(DIMENSION as u32) {
+        for y in 0..(DIMENSION as u32) {
             image.put_pixel(x, y, image::Rgba([0, 0, 0, 255]));
         }
     }
@@ -34,17 +34,25 @@ fn main() -> eyre::Result<()> {
 
     // draw
     for face in model.faces {
-        for i in 0..3 {
-            let v0 = model.vertices[face.indices[i] - 1];
-            let v1 = model.vertices[face.indices[(i + 1) % 3] - 1];
+        let vertices = face
+            .indices
+            .iter()
+            .map(|i| model.vertices[i - 1])
+            .map(|vertex| {
+                (
+                    ((vertex.x + 1f32) * (DIMENSION - 1) as f32 / 2f32) as i32,
+                    ((vertex.y + 1f32) * (DIMENSION - 1) as f32 / 2f32) as i32,
+                )
+            })
+            .collect::<Vec<_>>();
 
-            let x0 = ((v0.x + 1f32) * ((WIDTH-1) as f32) / 2f32) as i32;
-            let y0 = ((v0.y + 1f32) * ((HEIGHT-1) as f32) / 2f32) as i32;
-            let x1 = ((v1.x + 1f32) * ((WIDTH-1) as f32) / 2f32) as i32;
-            let y1 = ((v1.y + 1f32) * ((HEIGHT-1) as f32) / 2f32) as i32;
-
-            line(x0, y0, x1, y1, &mut image, WHITE);
-        }
+        filled_triangle(
+            vertices[0],
+            vertices[1],
+            vertices[2],
+            &mut image,
+            image::Rgba([rand::random(), rand::random(), rand::random(), 255]),
+        );
     }
 
     // encode and write to file
@@ -53,15 +61,15 @@ fn main() -> eyre::Result<()> {
 
     encoder.encode(
         image.flipv().as_bytes(),
-        WIDTH as u32,
-        HEIGHT as u32,
+        DIMENSION as u32,
+        DIMENSION as u32,
         image::ColorType::Rgba8,
     )?;
 
     Ok(())
 }
 
-#[derive(Clone, Copy, PartialEq, Debug)]
+#[derive(Clone, Copy, PartialEq, Debug, PartialOrd)]
 pub struct Vec3 {
     x: f32,
     y: f32,
